adafd844269d25e70c8f5b0a7c6fc53d27c442a8
gomeswes <gomes.wesley@gmail.com>
2015-06-25 08:36:11 -0300

    Comoom directives
    ng-if, ng-show, ng-hide, ng-loop, $scope.$apply

--- FirstSteps/index.js
+++ FirstSteps/index.js
@@ -5,9 +5,37 @@ var myApp = angular.module("myApp", []);
 myApp.controller("mainController", ['$scope', '$log', '$timeout', '$filter', function($scope, $log, $timeout, $filter) {
     "use strict";
     $scope.handle = "";
-    $scope.lowercasehandle = function(){
+    $scope.characters = 5;
+    $scope.lowercasehandle = function() {
         return $filter('lowercase')($scope.handle);
     };
+    
+    $scope.$watch('handle', function(newValue, oldValue) {
+        $log.debug(oldValue);
+        $log.debug(newValue);
+    });
+    
+    setTimeout(function(){
+        $scope.$apply(function(){
+            $scope.handle = 'handlechanged';
+            $log.info('scope changed');
+        });
+    }, 3000);
+    
+    $timeout(function(){
+        $scope.handle = "again-insidescope";
+        $log.info('inside scope');
+    }, 5000);
+    
+    $scope.rulez = 
+    [
+        { rulename: 'Must be repeated' },
+        {rulename: 'And i will repeat again'},
+        {rulename: 'And again'},
+        {rulename: 'And then again'},
+        {rulename: 'Ok! No more rulez' }
+    ];
+    
 }]);
 
 
