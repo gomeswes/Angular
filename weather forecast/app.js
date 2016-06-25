d1a1a8d4c7db6d1ba4dde96e737241b3a2beaee7
Wesley Gomes <Wesley Gomes>
2016-06-25 13:37:00 +0200

    custom service and biding

--- weather forecast/app.js
+++ weather forecast/app.js
@@ -15,14 +15,27 @@ weatherApp.config(function($routeProvider){
         })
 });
 
-// CONTROLLERS
-weatherApp.controller('homeController', ['$scope', function($scope) {
+// SERVICES
+weatherApp.service('cityService', function(){
+    this.city = 'New York, NY';
+})
 
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
 
-
+                                             
+    $scope.city = cityService.city;
+                                       
+                                             
 }]);
 
